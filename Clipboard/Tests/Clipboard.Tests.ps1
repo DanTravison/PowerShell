88e252f7398687647e71159b3038691f22b303a7
DanTravison <dan.travison@live.com>
2018-05-14 17:14:04 -0700

    Add Tests Update README

---
+++ Clipboard/Tests/Clipboard.Tests.ps1
@@ -0,0 +1,217 @@
+using namespace PowerShell.Clipboard
+Import-Module .\Clipboard.psd1
+
+Describe "Clipboard" -Tag @('CI') `
+{
+    Context 'Clear Clipboard' `
+    {
+        BeforeEach {
+            [Clipboard]::SetText("Clear Clipboard test", [ClipboardTextFormat]::Text)
+        }
+
+        It 'Verifies the clipboard is empty after [Clipboard]::Clear' `
+        {
+            [Clipboard]::Contains([ClipboardTextFormat]::Text) | Should -Be $true
+            [Clipboard]::Clear()
+            $formats = [Clipboard]::GetFormats()
+
+            $formats.Count | Should -Be 0
+        }
+
+        It 'Verifies the clipboard is empty after Clear-Clipboard' `
+        {
+            [Clipboard]::Contains([ClipboardTextFormat]::Text) | Should -Be $true
+            Clear-Clipboard
+            $formats = [Clipboard]::GetFormats()
+            $formats.Count | Should -Be 0
+        }
+    }
+
+    Context 'Set and Get-Clipboard' `
+    {
+        BeforeEach {
+            [Clipboard]::Clear()
+        }
+
+        It 'Verifies the clipboard can be set and retrieved as Unicode' `
+        {
+			$expected = 'Get-Clipbooard Text test'
+			Set-Clipboard -Value $expected -Unicode
+            $actual = Get-Clipboard -Unicode 
+			$actual | Should -Be $expected
+        }
+
+		It 'Verifies the clipboard can be set and retrieved as Text' `
+        {
+			$expected = 'Get-Clipbooard Text test'
+			Set-Clipboard -Value $expected -Text
+            $actual = Get-Clipboard -Text 
+			$actual | Should -Be $expected
+        }
+		
+		It 'Verifies the clipboard can be set and retrieved as HTML' `
+        {
+			$expected = @'
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>HTML TABLE</title>
+</head><body>
+<table>
+<colgroup><col/><col/><col/></colgroup>
+<tr><th>Name</th><th>Age</th><th>Friends</th></tr>
+<tr><td>John Doe</td><td>42</td><td>System.Object[]</td></tr>
+</table>
+</body></html>
+'@
+			Set-Clipboard -Value $expected -HTML
+            $actual = Get-Clipboard -HTML 
+			$actual | Should -Be $expected
+        }
+
+        It 'Verifies the clipboard can be set and retrieved as a FileList (CF_HDROP)' `
+        {
+            $expected = @(
+                [IO.Path]::Combine($TestDrive, "foo", "bar.txt"),
+                [IO.Path]::Combine($TestDrive, "bar", "bar.txt"),
+                [IO.Path]::Combine($TestDrive, "baz", "bar.txt")
+            )
+            Set-Clipboard -Values $expected -FileList
+            $actual = Get-Clipboard -FileList
+
+            $actual | Should -Not -BeNull
+            $actual.Count | Should -Be $expected.Count
+
+            for ($x = 0; $x -lt $expected.Count; $x++)
+            {
+                $exp = $expected[$x]
+                $act = $actual[$x]
+                $act | Should -Be $exp
+            }
+        }
+
+        It 'Verifies the clipboard can be set and retrieved as a XmlSpreadSheet' `
+        {
+            $expected = @'
+<?xml version="1.0"?>
+<?mso-application progid="Excel.Sheet"?>
+<Workbook xmlns="urn:schemas-microsoft-com:office:spreadsheet"
+ xmlns:o="urn:schemas-microsoft-com:office:office"
+ xmlns:x="urn:schemas-microsoft-com:office:excel"
+ xmlns:ss="urn:schemas-microsoft-com:office:spreadsheet"
+ xmlns:html="http://www.w3.org/TR/REC-html40">
+ <Styles>
+  <Style ss:ID="Default" ss:Name="Normal">
+   <Alignment ss:Vertical="Bottom"/>
+   <Borders/>
+   <Font ss:FontName="Calibri" ss:Color="#000000"/>
+   <Interior/>
+   <NumberFormat/>
+   <Protection/>
+  </Style>
+  <Style ss:ID="s15">
+   <Alignment ss:Vertical="Bottom"/>
+  </Style>
+  <Style ss:ID="s19">
+   <Alignment ss:Vertical="Bottom"/>
+   <NumberFormat ss:Format="mm/dd/yy;@"/>
+  </Style>
+  <Style ss:ID="s21">
+   <Alignment ss:Horizontal="Right" ss:Vertical="Bottom"/>
+   <NumberFormat ss:Format="[$-F400]h:mm:ss\ AM/PM"/>
+  </Style>
+  <Style ss:ID="s22">
+   <Alignment ss:Horizontal="Right" ss:Vertical="Bottom"/>
+  </Style>
+ </Styles>
+ <Worksheet ss:Name="BP">
+  <Table ss:ExpandedColumnCount="4" ss:ExpandedRowCount="8" ss:StyleID="s15"
+   ss:DefaultRowHeight="13.8">
+   <Column ss:StyleID="s19" ss:Width="54.599999999999994"/>
+   <Column ss:StyleID="s21" ss:Width="58.8"/>
+   <Column ss:StyleID="s22" ss:AutoFitWidth="0" ss:Width="49.2"/>
+   <Column ss:StyleID="s15" ss:AutoFitWidth="0" ss:Width="30.599999999999998"/>
+   <Row>
+    <Cell><Data ss:Type="DateTime">2016-08-24T00:00:00.000</Data></Cell>
+    <Cell><Data ss:Type="String">7:35pm</Data></Cell>
+    <Cell><Data ss:Type="String">138/76</Data></Cell>
+    <Cell><Data ss:Type="Number">71</Data></Cell>
+   </Row>
+   <Row>
+    <Cell><Data ss:Type="DateTime">2016-09-25T00:00:00.000</Data></Cell>
+    <Cell><Data ss:Type="String">8:15pm</Data></Cell>
+    <Cell><Data ss:Type="String">120/68</Data></Cell>
+    <Cell><Data ss:Type="Number">87</Data></Cell>
+   </Row>
+   <Row>
+    <Cell><Data ss:Type="DateTime">2016-10-16T00:00:00.000</Data></Cell>
+    <Cell><Data ss:Type="String">5:30pm</Data></Cell>
+    <Cell><Data ss:Type="String">132/73</Data></Cell>
+    <Cell><Data ss:Type="Number">61</Data></Cell>
+   </Row>
+  </Table>
+ </Worksheet>
+</Workbook>
+'@
+            Set-Clipboard -Value $expected -XmlSpreadsheet
+            $actual = Get-Clipboard -XmlSpreadsheet 
+			$actual | Should -Be $expected
+        }
+
+        It 'Verifies the clipboard can be set and retrieved as RTF' `
+        {
+            $expected = @'
+{\rtf\ansi{\fonttbl{\f0 Consolas;}}{\colortbl;\red0\green0\blue139;\red0\green0\blue0;\red138\green43\blue226;\red0\green0\blue255;}\f0 \fs19 \cf1 \cb0 \highlight0 using\cf2  \cf1 namespace\cf2  \cf3 PowerShell.Clipboard\cf2 \par \cf4 Import-Module\cf2  \cf3 .\\Clipboard.psd1}
+'@
+            Set-Clipboard -Value $expected -Rtf
+            $actual = Get-Clipboard -Rtf 
+			$actual | Should -Be $expected
+        }
+
+        It 'Verifies the clipboard can be set and retrieved as CSV' `
+        {
+            $jsonValue = @'
+{
+"one" : 1,
+"two" : 2,
+"three" : 3,
+"text" : "text"
+}
+'@
+            $expected = ConvertFrom-Json -InputObject $jsonValue | ConvertTo-Csv | Out-String
+            Set-Clipboard -Value $expected -CSV
+            $actual = Get-Clipboard -CSV 
+			$actual | Should -Be $expected
+        }
+
+    }
+
+    Context 'Get-ClipboardFormat' `
+    {
+        It 'Verifies expected formats' `
+        {
+            $expectedFormats = @('Unicode', 'Locale', 'Text', 'OemText')
+            Set-Clipboard -Value 'Get-ClipboardFormats' -Unicode
+
+            $formats = Get-ClipboardFormat
+            $actualFormats = @{}
+            foreach ($format in $formats)
+            {
+                $actualFormats[$format.Name, $format]
+            }
+
+            foreach ($name in $expected)
+            {
+                $value = $actualFormats[$name]
+                $value | Should -Be $name
+            }
+        }
+
+        It 'Verifies empty format list with clipboard is empty' `
+        {
+            Clear-Clipboard
+            $formats = Get-ClipboardFormat
+            $formats.Count | Should -Be 0
+        }
+    }
+}
--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Miscellaneous PowerShell modules and utilities.
 # Elevate Module
 This module is found in the Elevate directory.
 
-The module provides the capability for starting a PowerShell process elevated. 
+The module provides the capability for starting a PowerShell process elevated.
 
 On Windows, the process is started using ShellExecuteEx and causes a UAC prompt to be displayed.
 
@@ -21,7 +21,25 @@ This is useful for running commands that require interaction. The process will e
 * -NoExit: Places PowerShell in interactive mode after the command completes.
 This switch also sets the -ShowWindow switch.
 
-* Interctive only
-To launch a PowerShell process for interactive use, do not provide a -command parameter. Doing so implicitly sets the -NoExit and -ShowWindow switches. 
+* Interactive only
+To launch a PowerShell process for interactive use, do not provide a -command parameter. Doing so implicitly sets the -NoExit and -ShowWindow switches.
 
+# Clipboard Module
+This module supports access to the clipboard in PowerShell Core supporting some of the common, text-base clipboard formats including:
 
+* Unicode
+* Text
+* HTML
+* XmlSpreadSheet
+* RTF
+* CSF
+* FileList (HDROP)
+
+The Module defines the following commands:
+
+| Command             | Description |
+|---------------------|-------------|
+| Clear-Clipboard     | Clears the clipboard
+| Get-Clipboard       | Gets the clipboard in a specified format, if available. If no format is specified, the command attempts to choose the best format.
+| Get-ClipboardFormat | Lists the formats supported by the current clipboard contents.
+| Set-Clipboard       | Sets the contents of the clipboard to one of the above, supported formats.
